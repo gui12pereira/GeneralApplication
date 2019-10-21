8c5a3bce10fea2e8f85c7237e0d03f6ffe60b06a
Guilherme Pereira <guipereirasantos2002@gmail.com>
2019-10-21 16:57:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senai/generalapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.senai.generalapplication;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
