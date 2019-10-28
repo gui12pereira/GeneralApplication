3b48411a720e61ee72674980cc5c1ae5fc1b21d6
gui12pereira <guipereirasantos2002@gmail.com>
2019-10-28 15:56:26 -0300

    Without conexions and third activity

--- app/src/main/java/br/com/senai/generalapplication/MainActivity.java
+++ app/src/main/java/br/com/senai/generalapplication/MainActivity.java
@@ -1,10 +1,15 @@
 package br.com.senai.generalapplication;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -16,5 +21,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        //Sobrepõe a toolbar criada pela padrão
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //Criando um menu alternado a partir da instância ActionBarDrawerToggle, que recebe 5 parâmetros:
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this,
+                        drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_home){
+            Toast.makeText(this, "Entrada", Toast.LENGTH_SHORT ).show();
+        }
+        return false;
     }
 }
+
